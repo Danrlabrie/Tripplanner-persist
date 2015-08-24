246e4073de1b41a00ddfea4672204d3fa841e4b7
= <danrlabrie@gmail.com>
2015-08-24 12:35:02 -0400

    re-commit

--- routes/api/days.js
+++ routes/api/days.js
@@ -11,25 +11,20 @@ var Promise = require('bluebird');
 
 
 router2.get('/api/days', function(req, res) {
-  console.log(Day)
     Day.find()
     .then(function(days) {
-        res.render('days', {
-        all_days: days
+        res.json(days)
       });
     })
-})
 
 /// get a specific day
 
 router2.get('/api/days/:id', function(req, res) {
     Day.findById(req.params.id)
     .then(function(days) {
-        res.render('days', {
-        all_days: days
-      });
-    })
-})
+        res.json(day);
+    });
+});
 
 /// delete a specific day
 
@@ -44,61 +39,64 @@ router2.delete('/api/days/:id', function(req, res) {
 
 // create a new day
 
-router2.post('/api/days/:id', function(req, res) {
-    if(Day.findById(req.params.id)) return err
-    else var newday = new Day( {
+router2.post('/api/days/:id', function(req, res, next) {
+     var newday = new Day( {
       number: req.params.id
     })
     newday.save().exec()
-    .then(function(savedPage){
-    res.redirect(savedPage.route);
+    .then(function(){
+    res.json(newday);
   }).catch(next);
 });
 
 ///////////////// create specifics on days ////////////////////
 
 
-// create new Hotel on day
+// // create new Hotel on day
 
-router2.post('/api/days/:id/hotel', function(req, res) {
-   Day.findById(req.params.id)
-    .then(function(day) {
+// router2.post('/api/days/:id/hotel', function(req, res) {
+//    Day.findById(req.params.id)
+//     .then(function(day) {
+//         res.render('days', {
+//         singleday: day
+//       });
+//     })
+// })
 
-        res.render('days', {
-        singleday: day
-      });
-    })
+// // create new Restaurant on day
+
+// router2.post('/api/days/:id/restaurants', function(req, res) {
+//    Day.findById(req.params.id)
+//     .then(function(day) {
+
+//         res.render('days', {
+//         singleday: day
+//       });
+//     })
+// });
+
+// // create new Activity on day
+
+// router2.post('/api/days/:id/activities', function(req, res) {
+//    Day.findById(req.params.id)
+//     .then(function(day) {
+
+//         res.render('days', {
+//         singleday: day
+//       });
+//     })
+// });
+
+// //////////////////////////////////////////////////////////////
+
+// ////////////////// delete specifics on days /////////////////
+
+router2.use('/api',function (err, req, res, next) {
+  if(err) {
+    console.log(err);
+    res.send(err);
+  }
 })
-
-// create new Restaurant on day
-
-router2.post('/api/days/:id/restaurants', function(req, res) {
-   Day.findById(req.params.id)
-    .then(function(day) {
-
-        res.render('days', {
-        singleday: day
-      });
-    })
-});
-
-// create new Activity on day
-
-router2.post('/api/days/:id/activities', function(req, res) {
-   Day.findById(req.params.id)
-    .then(function(day) {
-
-        res.render('days', {
-        singleday: day
-      });
-    })
-});
-
-//////////////////////////////////////////////////////////////
-
-////////////////// delete specifics on days /////////////////
-
-
 
 
 module.exports = router2;
